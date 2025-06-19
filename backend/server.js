88c9a4c9d9cf6cbbb2ebc6b2838bad75a91c6972
Archit743 <architaggupta@gmail.com>
2025-06-20 00:19:25 +0530

    fixed a cors issue

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({
-  origin: 'https://campus-cupid-lake.vercel.app',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
